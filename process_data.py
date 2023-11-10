5a8242af09bcf003b1fde336a509688d8779c1e2
PedroAMtz <pedromartinez.dg2@gmail.com>
2023-11-09 19:39:27 -0600

    Process data and some feature extraction

--- process_data.py
+++ process_data.py
@@ -17,38 +17,6 @@ def loadEMGs(ruta: str) -> Tuple[np.ndarray, np.ndarray]:
     t = np.linspace(0, T, Fs * T)
     return np.array(emgs), t
 
-def process_signal(signal: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # Parámetros de la señal
-    Fs = 200
-    T = 120
-    ventanas = 22
-    windowsize = 150
-
-    # Cálculos iniciales
-    dV = (Fs * T) / ventanas
-    N = Fs * T
-    n = np.arange(N)
-    T = N / Fs
-    freq = n / T
-    frecN = int((Fs * T) / 2)
-
-    # Transformada rápida de Fourier
-    X = fft(signal)
-    emg_fft_abs = np.abs(X)
-    emg_fft_abs[0] = 0
-
-    # Envolvente superior
-    df = pd.DataFrame(data={"y": emg_fft_abs})
-    df["y_upperEnv"] = df["y"].rolling(window=windowsize).max().shift(int(-windowsize / 2))
-    envolvente_superior = df['y_upperEnv'].to_numpy()
-
-    return envolvente_superior, emg_fft_abs, freq
-
-def smooth_signal(envolvente: np.ndarray):
-
-    smoother_signal = savgol_filter(envolvente, 1001, 5)
-    return smoother_signal
-
 def plot_emg_window(emg_,n,dV,smooth_window=150,order=2,int_freqs=False):
     p_inicio = int(n*dV)
     p_final  = int((n+1)*dV)
@@ -78,24 +46,18 @@ def plot_emg_window(emg_,n,dV,smooth_window=150,order=2,int_freqs=False):
 
     return freq, suavizada
 
+
 def extract_features(freq, suavizada):
     amplitude_rms = np.sqrt(np.mean(np.square(suavizada)))
 
     freq_media = np.mean(freq * suavizada)
 
     freq_mediana = np.median(freq * suavizada)
-
-    mask_0_50 = (freq >= 0) & (freq < 50)
-    mask_50_100 = (freq >= 50) & (freq < 100)
-    potencia_0_50 = np.sum(suavizada[mask_0_50])
-    potencia_50_100 = np.sum(suavizada[mask_50_100])
-    ratio_potencia = potencia_0_50 / potencia_50_100
     
     features = {
         'amplitude_rms': amplitude_rms,
         'freq_media': freq_media,
-        'freq_mediana': freq_mediana,
-        'ratio_potencia': ratio_potencia
+        'freq_mediana': freq_mediana
     }
 
     return features
@@ -105,7 +67,7 @@ if __name__ == "__main__":
     # Parámetros/variables globales
     Fs = 200
     tT = 120
-    N_ventanas = 22
+    N_ventanas = 15
     dV = (Fs*tT)/N_ventanas
 
     N = Fs*tT
@@ -116,18 +78,43 @@ if __name__ == "__main__":
     frecN = int((Fs*T)/2)
 
     emgs, time = loadEMGs("selected_emgs/")
-
+    df = pd.DataFrame()
     plt.figure(figsize=(15,12))
 
+    segmentos_totales = []
+    frecuencias_totales = []
     for m in range(10):
+        segmentos = []
+        frecuencias = []
         for n in range(N_ventanas):
-            if n > 0 and n < N_ventanas-1:
-                continue
-            plt.subplot(5,2,m+1)
             freq, suavizada = plot_emg_window(emgs[m],n,dV,smooth_window=500,int_freqs=True)
-            features = extract_features(freq, suavizada)
-            print(features)
 
-        plt.title('EMG {}'.format(m+1),fontsize=10)
-        plt.legend()
-    plt.show()
+            features = extract_features(freq, suavizada)
+
+            data = {
+            'señal_suavizada': suavizada,
+            'frecuencia': freq[np.argmax(suavizada)],
+            'amplitude_rms': features['amplitude_rms'],
+            'freq_media': features['freq_media'],
+            'freq_mediana': features['freq_mediana']
+        }
+            df = df._append(data, ignore_index=True)
+            frecuencias.append(freq[np.argmax(suavizada)])
+            segmentos.append(suavizada)
+        segmentos_totales.append(segmentos)
+        frecuencias_totales.append(frecuencias)
+    
+    etiquetas = []
+
+    # crieterio para etiquetado, básicamente el decremento en frecuencia
+    for value in frecuencias_totales:
+        for j in range(len(value)):
+            etiqueta = 0 if value[0] - value[j] > 8 else 1
+            etiquetas.append(etiqueta)
+
+    print(len(etiquetas))
+    df["etiquetas"] = etiquetas
+    print(df.head(16))
+    
+    df.to_csv("data/processed_data.csv", index=False)
+    
